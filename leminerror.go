077de3c16a1c842a1400519d327266c48205becf
BURAK <hazirburakcan@gmail.com>
2024-05-22 13:02:34 +0300

    lem-in-ERROR

--- leminerror.go
+++ leminerror.go
@@ -8,7 +8,6 @@ func IsFileTypeCorrect(givenFileName string) bool {
 	extension := path.Ext(givenFileName)
 	return extension == ".txt"
 }
-
 func CleanTheComments(seperatedContent []string) []string {
 	var res []string
 	for i := 0; i < len(seperatedContent); i++ {
